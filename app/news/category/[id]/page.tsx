835391ab94b90edd76797bda6ffbdcdebe0a490b
na0chan-go <nn19960218@gmail.com>
2024-07-16 23:46:08 +0900

    fix: typo

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -24,7 +24,7 @@ export default async function Page({ params }: Props) {
       <p>
         <Category category={category} /> の一覧
       </p>
-      <NewsList news={news} />;
+      <NewsList news={news} />
       <Pagination
         totalCount={totalCount}
         basePath={`/news/category/${category.id}`}

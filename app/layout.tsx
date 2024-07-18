26179c59b8b335fc550aec16e0994b0dcaa83837
na0chan-go <nn19960218@gmail.com>
2024-07-19 00:35:10 +0900

    feat: アナリティクスの設定を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
@@ -34,6 +35,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-PXBQ69DCB4" />
     </html>
   );
 }
